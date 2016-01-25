5b47672df3321695858c027f91237a8e7f818611
Micah Wells <micahp0506@gmail.com>
2016-01-25 15:29:23 -0600

    Added additional tests

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -7,6 +7,35 @@ describe('template strings', function () {
 
     expect(string).to.be.a('string');
   });
-});
+
+  it('can evaluate variables', function () {
+    var bar = `baz`;
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+  });
+
+  it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`
+    var date = new Date();
+
+    expect(string).to.equal('The date is: ' + date);
+  });
+
+  it('can evaluate expressions', function () {
+    var string = `The answer is: ${1+2}`
+    var answer = 3;
+
+    expect(string).to.equal('The answer is: ' + answer);
+  });
+
+  it('can evaluate multiline strings', function () {
+    var string = `Hello
+World`;
+    expect(string).to.equal('Hello\n'+'World');
+  });
+
+
+ });
 
 
